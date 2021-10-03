fd580876eb69ce4a235413224986c07f36b24565
christianCoyto <christiancoyto@gmail.com>
2021-10-03 00:15:42 -0300

    clase scroll

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var header = document.getElementById('Header')
 
-window.addEventListener("scroll", () =>{
-    var scroll = window.scrollY;
-    if(scrollY>0){
+/* window.addEventListener("scroll", () =>{
+    if(window.scrollY > 0){
         header.style.backgroundColor = 'rgba(64, 65, 57, 82%)';
 
-    }
-    else{
+    }else{
         header.style.backgroundColor = 'transparent';
     }
+}) */
 
+window.addEventListener("scroll", () => {
+    header.classList.toggle('scroll', (window.scrollY > 0));
 })
